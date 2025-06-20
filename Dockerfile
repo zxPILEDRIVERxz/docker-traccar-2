bdad646840254f9a93ca10bf558855659e7b38d0
Daniel Pile <dpile103@gmail.com>
2025-06-20 11:09:07 -0400

    configure database connection

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,12 @@ RUN chmod -R 777 /opt/traccar/logs /opt/traccar/conf
 # Copy run script
 #COPY rootfs /
 
+# Install dependencies
+RUN \
+    apk update && \
+    apk add --no-cache \
+        mariadb-client \
+        xmlstarlet
+
 # Expose ports: 8082 (Traccar web UI), 5055 (OsmAnd protocol)
 EXPOSE 8082 5000-5150 5000-5150/udp
---
+++ rootfs/etc/cont-init.d/traccar.sh
@@ -0,0 +1,26 @@
+#!/command/with-contenv bashio
+# ==============================================================================
+# Home Assistant Community Add-on: Traccar
+# Ensures the user configuration file is present
+# ==============================================================================
+declare host
+declare password
+declare port
+declare username
+
+# Migrate add-on data from the Home Assistant config folder,
+# to the add-on configuration folder.
+
+# Existing installation
+if bashio::services.available "mysql"; then
+    # Make sure the database isn't locked
+    host=$(bashio::services "mysql" "host")
+    password=$(bashio::services "mysql" "password")
+    port=$(bashio::services "mysql" "port")
+    username=$(bashio::services "mysql" "username")
+
+    echo "UPDATE DATABASECHANGELOGLOCK SET locked=0;" \
+        | mysql -h "${host}" -P "${port}" -u "${username}" -p"${password}" \
+            traccar || true
+fi
+
